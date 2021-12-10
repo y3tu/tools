d62df68a93bd101fbd443bc53350fd8a02b9b155
y3tu <14643092@qq.com>
2021-12-10 20:32:59 +0800

    update & fix bug & format code

---
+++ tools-lowcode/src/main/webapp1/test/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <router-view #="{ Component, route }">
+    <component :is="Component" :key="route.path" />
+  </router-view>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss">
+@import 'style/common';
+</style>
---
+++ tools-lowcode/src/main/webapp1/test/src/main.ts
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+
+import router from './router/'
+import store from './store/'
+
+const app = createApp(App)
+
+// 使用element-plus插件
+import { setupElementPlus } from './plugins/element-plus'
+setupElementPlus(app)
+
+app.use(router).use(store)
+// 路由准备完毕再挂载
+router.isReady().then(() => app.mount('#app'))
